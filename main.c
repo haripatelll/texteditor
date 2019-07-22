81153bf04fd0154421b95f5a4fbd1e8fae918807
haripatelll <34925349+haripatelll@users.noreply.github.com>
2019-07-22 00:52:40 -0400

    Adding Terminal View
    Adding Terminal View

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "include.h"
+#include "terminal.h"
+
+int main() 
+{
+  enableecho();
+  while (1) {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) kill("read");
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q') break;
+  }
+  return 0;
+}
---
+++ terminal.h
@@ -0,0 +1,15 @@
+#include "include.h"
+
+struct termios TERMINAL;
+
+void kill(const char *s) 
+{
+}
+
+void disablecho() 
+{
+}
+
+void enableecho() 
+{
+}
