4ef95dacea4d9dcbc6d8b70655b543073eaed0c9
haripatelll <34925349+haripatelll@users.noreply.github.com>
2019-07-31 00:55:30 -0400

    Added Coordinate to struct

--- info.h
+++ info.h
@@ -4,6 +4,8 @@ This file contains all the structs and overall variables
 #include <termios.h>
 
 struct base_editor {
+	int x_coor;
+	int y_coor;
 	int row;
 	int column;
 	struct termios terminall;
