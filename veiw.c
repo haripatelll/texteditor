4848c4b04e9432498a302115e3f108a552d87ba0
haripatelll <34925349+haripatelll@users.noreply.github.com>
2019-07-31 00:55:16 -0400

    padder and window config

--- veiw.c
+++ veiw.c
@@ -14,23 +14,56 @@ void esc_cursor()
 }
 
 
-void row_chars() 
+void row_chars(struct dynamicbuff *db) 
 {  
 	int row_count;
 	for (row_count = 0; row_count < editor.row; row_count++) 
 	{
-		write(STDOUT_FILENO, startline, 1);
+		// Intro
+		if (row_count == editor.row / 3)
+		{
+			char intro[80];
+			int sizee = sizeof(intro);
+			int introleng = snprintf(intro, sizee, "Tiny Texteditor");
+			if (introleng > editor.column)
+			{
+				introleng = editor.column;
+			}
+			int pad = (editor.column - introleng) / 2;
+			if (pad) 
+			{
+				cons_dynamic(db, startline, 1);	
+				pad--;
+			}
+			while (pad--)
+			{
+				cons_dynamic(db, " ", 1);					
+			}
+			cons_dynamic(db, intro, introleng);	
+		} 
+		else 
+		{
+			cons_dynamic(db, startline, 1);			
+		}
+		cons_dynamic(&db, "\x1b[K", 4);
 		if (row_count < editor.row - 1) 
 		{
-			write(STDOUT_FILENO, "\r\n", 2);
+			cons_dynamic(STDOUT_FILENO, row_print, 2);
 		}
 	}
 }  
 
+
 void screen_refresh()
-{
-	esc_clear();
-	esc_cursor();
-	row_chars();
-	esc_cursor();
+{  
+	struct dynamicbuff db = DYNAMIC;
+	// Set Mode
+	cons_dynamic(&db, "\x1b[?25l", 6);
+	cons_dynamic(&db, esc_sequence_cursor, 3);
+	row_chars(&db);
+	cons_dynamic(&db, esc_sequence_cursor, 3);
+	// Reset Mode
+	cons_dynamic(&db, "\x1b[?25h", 6);
+	write(STDOUT_FILENO, db.buff, db.leng);
+	des_dynamic(&db);
 }
