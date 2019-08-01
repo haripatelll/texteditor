b7b1175050d0c1861fc3f791a290c8e4baa129e7
haripatelll <34925349+haripatelll@users.noreply.github.com>
2019-08-01 00:42:31 -0400

    Added the keys with values complete

--- include.h
+++ include.h
@@ -11,6 +11,16 @@
 // Bitwise-ANDs a character with the value 00011111
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+// Designates the keys to characters
+enum arrowkeys 
+{
+  LEFT_ARROW = 1000, RIGHT_ARROW = 1001, 
+  UP_ARROW = 1002, DOWN_ARROW = 1003,
+  UP_PAGE = 1004, DOWN_PAGE = 1005,
+  HOME  = 1006, END = 1007,
+  DELETE = 1008
+};
+
 /* 
 Dynmaic String 
 This Is an Append Buffer
