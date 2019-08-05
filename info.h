409d5405d1733f394ba21dd3a84798c3f67c4794
haripatelll <34925349+haripatelll@users.noreply.github.com>
2019-08-05 01:29:28 -0400

    Update info.h

--- info.h
+++ info.h
@@ -3,11 +3,19 @@ This file contains all the structs and overall variables
 */
 #include <termios.h>
 
+// Allows us to store the data for the text in the editor
+typedef struct editor_row {
+  int size;
+  char *data;
+} editor_row;
+
 struct base_editor {
 	int x_coor;
 	int y_coor;
 	int row;
 	int column;
+	int rowcount;
+	editor_row rows;
 	struct termios terminall;
 };
 
