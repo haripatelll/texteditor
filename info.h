9e5e1eb033776cc4616dae6c9e0217e889ecd740
haripatelll <34925349+haripatelll@users.noreply.github.com>
2019-08-05 15:53:17 -0400

    File and tab information added

--- info.h
+++ info.h
@@ -1,19 +1,22 @@
 /*
 This file contains all the structs and overall variables
 */
+#include <time.h>
 #include <termios.h>
 
 // Allows us to store the data for the text in the editor
 typedef struct editor_row {
-  int size;
-  char *data;
-  int tabsize;
-  char *tabrend;
+	int size;
+	char *data;
+	int tabsize;
+	char *tabrend;
 } editor_row;
 
 struct base_editor {
 	int x_coor;
 	int y_coor;
+	// cursor tab fix
+	int rx_coor;
 	int row;
 	int column;
 	int rowcount;
@@ -23,12 +26,15 @@ struct base_editor {
 	int column_offset;
 	// dynamic rows
 	editor_row *rows;
+	char *file;
+	char statmesg[80];
+	time_t statmesg_time;
 	struct termios terminall;
 };
 
 struct base_editor editor;
 
 struct dynamicbuff {
-  char *buff;
-  int leng;
+	char *buff;
+	int leng;
 };
