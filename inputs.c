76c262c982b58687b201bd23600733765e0c4d92
haripatelll <34925349+haripatelll@users.noreply.github.com>
2019-08-05 23:26:08 -0400

    Prompt added

--- inputs.c
+++ inputs.c
@@ -1,6 +1,52 @@
 #include "inputs.h"
 
 
+char *editorPrompt(char *prompt) 
+{
+	int buffersize = 128;
+	char *buffer = malloc(buffersize);
+	int bufferleng = 0;
+	buffer[0] = '\0';
+	while (1) 
+	{
+		setstatus(prompt, buffer);
+		screen_refresh();
+		int ch = read_editor();
+		if (ch == DELETE || c == CTRL_KEY('h') || c == BACKSPACE) 
+		{
+			if (bufferleng != 0)
+			{ 
+				buffer[--bufferleng] = '\0';
+			}
+		} 
+		else if (ch == '\r') 
+		{
+			if (bufferleng != 0) 
+			{
+				setstatus("");
+				return buffer;
+			}
+		} 
+		else if (ch == '\x1b') 
+		{
+			setstatus("");
+			free(buffer);
+			return NULL;
+		} 
+		else if (!iscntrl(ch) && ch < 128) 
+		{
+			if (bufferleng == buffersize - 1) 
+			{
+				buffersize *= 2;
+				buffer = realloc(buffer, buffersize);
+			}
+			buffer[bufferleng++] = ch;
+			buffer[bufferleng] = '\0';
+		}
+	}
+}
+
+
 void cursormovement_editor(int keys)
 {
 	editor_row *currow = (editor.rowcount <= editor.y_coor) ? NULL : &editor.rows[editor.y_coor];
@@ -58,11 +104,30 @@ void process_editor()
 {  
 	int ch = read_editor();
 	int boundcheck;
+	int quits = QUIT_COUNT;
 	switch (ch) {
 		case CTRL_KEY('q'):
+		if (editor.modified && quits > 0) 
+		{
+			setstatus("Unsaved, %s Changes. Press Ctrl-Q %d more times to quit.", editor.modified, quits);
+			quits--;
+			return;
+		}
 		esc_clear();
 		esc_cursor();
 		exit(0);
+		break;
+		case CTRL_KEY('s'):
+		save();
+		break;
+		case CTRL_KEY('h'):
+		break;
+		case CTRL_KEY('l'):
+		break;		
+		case '\r':
+		insertendline();
+		break;    
+		case '\x1b':
 		break;
 		case UP_ARROW:
 		break;
@@ -103,6 +168,19 @@ void process_editor()
 			editor.x_coor = editor.rows[editor.y_coor].size;
 		}
 		break;
+		case BACKSPACE:
+		break;
+		case DELETE:
+		if (c == DELETE)
+		{
+			cursormovement_editor(RIGHT_ARROW);
+		}
+		deletechar();
+		break;
+		default:
+		insertchar(ch);
+		break;
 	}
+	quits = QUIT_COUNT;
 }
 
--- inputs.h
+++ inputs.h
@@ -4,6 +4,10 @@ This file contains all the information for inputs given to the editor
 #include "include.h"
 #include "terminal.h"
 #include "view.h"
+#include "row.h"
+
+// Prompt user to input a filename when saving a new file
+char *userprompt(char *prompt);
 
 // Allows us to shift the cursor position
 void cursormovement_editor(int arrowkeys);
